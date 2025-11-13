fb72693dbee1b084a210487a811b65e6bc83a850
adhyastanf <adhyasta.nf@gmail.com>
2025-11-13 09:59:23 +0700

    fix build prod

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${geistMono.variable} ${geistMono.variable} antialiased overflow-hidden overscroll-none`}>
+      <body className={`${geistMono.variable} antialiased overflow-hidden overscroll-none`}>
         <Navmenu />
         {children}
         <Toaster />
